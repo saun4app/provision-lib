ca09e4104f50145f59b2bec0ba083ccf47de1123
Saun Shewanown <saun4app@gmail.com>
2018-05-28 07:28:11 -0400

    Add docker and nodejs

---
+++ bash-lib/nodejs-u1804.sh
@@ -0,0 +1,27 @@
+sudo apt-get install -y build-essential
+
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+node -v
+npm -v
+
+
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update -y
+sudo apt-get install -y yarn
+yarn -v
+
+# yarn global add gulp-cli
+
+# npm config get prefix
+# mkdir ~/.npm-global
+# npm config set prefix '~/.npm-global'
+# gedit ~/.profile
+# export PATH=~/.npm-global/bin:$PATH
+# source ~/.profile
+# npm i -g gulp-cli
+
+# sudo npm install -g yo
