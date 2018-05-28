c97c9d2b2e3ad37c685eb60dcf0d823a52fdd959
Saun Shewanown <saun4app@gmail.com>
2018-05-28 08:04:35 -0400

    Update docker

--- bash-lib/docker-ce-u1804.sh
+++ bash-lib/docker-ce-u1804.sh
@@ -1,11 +1,11 @@
 sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
-sudo apt-get update  -y
-sudo groupadd docker
-sudo usermod -aG docker $USER
-docker -v
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo apt update -y
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# docker -v
+
+
+# https://linuxconfig.org/how-to-install-docker-on-ubuntu-18-04-bionic-beaver
