5d4b1d6b83ec36b7b63ca75a6cc9306b92a76ca3
Saun Shewanown <saun4app@gmail.com>
2018-05-28 09:38:30 -0400

    Update portainer

--- bash-lib/docker-portainer-u1804.sh
+++ bash-lib/docker-portainer-u1804.sh
@@ -1,9 +1,1 @@
- wget https://portainer.io/assets/download/docker-compose.yml -P ~vagrant/program-dir/portainer-dir
- 
- # pkexec --user vagrant 'echo $USER'
-
-# pkexec --user vagrant 'wget https://portainer.io/assets/download/docker-compose.yml -P ~vagrant/program-dir/portainer-dir'
-
-# wget https://portainer.io/assets/download/docker-compose.yml -P ~vagrant/program-dir/portainer-dir
-# wget https://portainer.io/assets/download/docker-compose.yml -P /home/vagrant/program-dir/portainer-dir
-# wget https://portainer.io/assets/download/docker-compose.yml -P ~/program-dir/portainer-dir
+wget https://portainer.io/assets/download/docker-compose.yml -P ~vagrant/program-dir/portainer-dir
