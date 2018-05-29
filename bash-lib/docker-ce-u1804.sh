5cced6b502dff851d6c7487d8410876bb4c4be92
Saun Shewanown <saun4app@gmail.com>
2018-05-29 16:19:35 -0400

    Update docker-ce

--- bash-lib/docker-ce-u1804.sh
+++ bash-lib/docker-ce-u1804.sh
@@ -5,10 +5,11 @@ sudo systemctl enable docker
 sudo apt update -y
 
 sudo groupadd docker
-sudo gpasswd -a $USER docker
+sudo gpasswd -a vagrant docker
 newgrp docker
 docker -v
 
+# sudo gpasswd -a $USER docker
 
 # https://linuxconfig.org/how-to-install-docker-on-ubuntu-18-04-bionic-beaver
 # https://linoxide.com/linux-how-to/use-docker-without-sudo-ubuntu/
